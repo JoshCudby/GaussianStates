628d8382ef9a6c173e98acb00e86212c9d877a40
Josh Cudby <jjcc2@cam.ac.uk>
2022-10-19 17:46:24 +0100

    Code tidy up

--- Code/Constraints/GaussianConstraints.py
+++ Code/Constraints/GaussianConstraints.py
@@ -1,65 +1,206 @@
-import copy
-from ..States.GaussianStates import *
-from ..Utils.BinaryStringUtils import *
+from ..Utils.BinaryStringUtils import strings_with_weight, read_binary_array, int_to_binary_array
 import numpy as np
 import math
+from typing import List
+import random
 
 
-def get_highest_order_constraints(n, parity_of_targets: int):
-    if parity_of_targets != 0 and parity_of_targets != 1:
-        print('Parity must be either 0 or 1')
-        exit()
-    print(f'Starting for n = {n}')
-    constraints_matrix = np.zeros((2 ** (n - 2), 2 ** n, 2))
+def get_highest_order_constraints_even_case(n: int, parity_of_state: int) -> List[np.ndarray]:
+    if n % 2 != 0:
+        raise Exception('Only get highest order constraints for even n')
+    if parity_of_state != 0 and parity_of_state != 1:
+        raise Exception('Parity should be 0 or 1')
+
+    constraints_list: List[np.ndarray] = [0] * (2 ** (n - 2))
+    max_weight = 2 * math.floor((n + 2) / 4) if parity_of_state == 0 else 2 * math.floor(n / 4) + 1
     count = 0
-    max_weight = 2 * math.floor((n + 2) / 4) if parity_of_targets == 1 else 2 * math.floor((n - 1) / 4)
-    for k in range(parity_of_targets, max_weight, 2):
-        targets = strings_with_weight(n, k)
+    for k in range(1 - parity_of_state, max_weight, 2):
+        targets = [[0] * n] if k == 0 else strings_with_weight(n, k)
+
+        if k == n / 2:
+            targets = targets[0:int(len(targets) / 2)]
+
         for target in targets:
+            constraint = np.zeros((n, 2), dtype=int)
             for i in range(n):
-                x = copy.deepcopy(target)
+                x = np.copy(target)
                 x[i] = (x[i] + 1) % 2
                 y = (x + 1) % 2
                 x_int = read_binary_array(x)
                 y_int = read_binary_array(y)
                 sorted_term = sorted([x_int, y_int])
-                constraints_matrix[count, sorted_term[0], 0] = (-1) ** i
-                constraints_matrix[count, sorted_term[1], 1] = 1  # this is not a good way to represent
+                constraint[i, :] = sorted_term
+            constraints_list[count] = constraint
             count += 1
-
-    return constraints_matrix
-
-
-def get_lower_order_constraints(n, l):
-    # l is number of bits where the tensor product of targets differ
-    odd_weight_target_constraints = get_highest_order_constraints(l, 1)
-    even_weight_target_constraints = get_highest_order_constraints(l, 0)
-    new_constraints = []  # TODO make numpy
-
-    for i in range(0, n - l + 1, 2):
-        bit_strings = [0] * (n - l) if i == 0 else strings_with_weight(n - l, i)
-        # Need to find a way to correctly add the string where they agree to the constraint
-        for string in bit_strings:
-            for constraint in odd_weight_target_constraints:
-                for j in range(2 ** (n - l)):
-                    if constraint[j, 0] != 0:
-                        first_label_bin = int_to_binary_array(j, n - l)
-                        summed_first_label = np.array(np.meshgrid(string, first_label_bin))
-
-                        second_label_bin = int_to_binary_array()
-                        for summed_str in summed_first_label:
-                            constraint_term_label = read_binary_array(summed_str)
-
-            new_constraints.append(odd_weight_target_constraints)
-
-    return new_constraints
+    return constraints_list
 
 
-def verify_highest_order_constraints(n, constraints):
-    N = 2 ** n
-    gaussian = nn_gaussian_states(1, n, 0)
+def get_lower_order_constraints(constraints: List[np.ndarray], m: int) -> List[np.ndarray]:
+    all_new_constraints = []
+    # Loop over: constraints, even weight strings of length 2, choices of how to position
     for constraint in constraints:
-        result = 0
-        for i in range(len(constraint)):
-            result += constraint[i] * gaussian[i] * gaussian[N - i - 1]
-        np.testing.assert_almost_equal(result, 0)
+        constraint_in_binary = [[int_to_binary_array(int(t), m - 2) for t in term] for term in constraint]
+        length = len(constraint_in_binary[0][0])
+        new_constraints_in_binary = [
+            [[np.insert(binary_array, [i, j], b) for binary_array in term] for term in constraint_in_binary]
+            for i in range(length + 1)
+            for j in range(i, length + 1)
+            for b in [[0, 0], [1, 1]]
+        ]
+        new_constraints = [
+            [[read_binary_array(binary_array) for binary_array in term] for term in constraint]
+            for constraint in new_constraints_in_binary
+        ]
+        for new_constraint in new_constraints:
+            all_new_constraints.append(np.array(new_constraint))
+    return all_new_constraints
+
+
+def sorting_key(to_sort):
+    return to_sort[0]
+
+
+def remove_duplicates(constraints: List[np.ndarray]) -> List[np.ndarray]:
+    # This removes identical elements
+    seen_elements = set()
+    unique = []
+    for constraint in constraints:
+        sorted_constraint = tuple(map(tuple, sorted(constraint, key=sorting_key)))
+        if sorted_constraint not in seen_elements:
+            unique.append(constraint)
+            seen_elements.add(sorted_constraint)
+    return unique
+
+
+def get_highest_order_constraints_odd_case(n: int) -> List[np.ndarray]:
+    all_new_constraints = []
+    even_parity_constraints = remove_duplicates(get_highest_order_constraints_even_case(n - 1, 0))
+    odd_parity_constraints = remove_duplicates(get_highest_order_constraints_even_case(n - 1, 1))
+
+    # Loop over: constraints, strings of length 1, choices of how to position
+    for constraint in even_parity_constraints:
+        constraint_in_binary = [[int_to_binary_array(int(t), n - 1) for t in term] for term in constraint]
+        length = len(constraint_in_binary[0][0])
+        new_constraints_in_binary = [
+            [[np.insert(binary_array, i, 0) for binary_array in term] for term in constraint_in_binary]
+            for i in range(length + 1)
+        ]
+        new_constraints = [
+            [[read_binary_array(binary_array) for binary_array in term] for term in constraint]
+            for constraint in new_constraints_in_binary
+        ]
+        for new_constraint in new_constraints:
+            all_new_constraints.append(np.array(new_constraint))
+
+    for constraint in odd_parity_constraints:
+        constraint_in_binary = [[int_to_binary_array(int(t), n - 1) for t in term] for term in constraint]
+        length = len(constraint_in_binary[0][0])
+        new_constraints_in_binary = [
+            [[np.insert(binary_array, i, 1) for binary_array in term] for term in constraint_in_binary]
+            for i in range(length + 1)
+        ]
+        new_constraints = [
+            [[read_binary_array(binary_array) for binary_array in term] for term in constraint]
+            for constraint in new_constraints_in_binary
+        ]
+        for new_constraint in new_constraints:
+            all_new_constraints.append(np.array(new_constraint))
+
+    return all_new_constraints
+
+
+def get_all_constraints(n: int) -> List[np.ndarray]:
+    if n < 4:
+        return []
+    parity = n % 2
+
+    if parity == 0:
+        all_constraints = remove_duplicates(get_highest_order_constraints_even_case(4, 0))
+        for m in range(6, 2 * math.floor(n / 2) + 1, 2):
+            all_constraints = get_highest_order_constraints_even_case(m, 0) \
+                              + remove_duplicates(get_lower_order_constraints(all_constraints, m))
+            # print(f'For n={m}, there are {len(all_constraints)} constraints')
+    else:
+        c = get_highest_order_constraints_odd_case(5)
+        all_constraints = remove_duplicates(c)
+        for m in range(7, n + 1, 2):
+            all_constraints = get_highest_order_constraints_odd_case(m) \
+                              + remove_duplicates(get_lower_order_constraints(all_constraints, m))
+            # print(f'For n={m}, there are {len(all_constraints)} constraints')
+    return all_constraints
+
+
+def find_independent_constraints(all_constraints: List[np.ndarray], state: np.ndarray) -> List[np.ndarray]:
+    independent_constraints = []
+    for z in range(len(all_constraints)):
+        test_constraints = list(independent_constraints.copy())
+        test_constraints.append(all_constraints[z])
+        m = len(test_constraints)
+        flattened_constraints = [constraint.flatten() for constraint in test_constraints]
+        a_labels = [item for sublist in flattened_constraints for item in sublist]
+
+        a_labels_set = list(set(a_labels))
+        random.shuffle(a_labels_set)
+        # a_labels_set = sorted(list(set(a_labels)))
+
+        x_values = a_labels_set[0:m]
+        jacobian = np.zeros((m, m), dtype=complex)
+        for i in range(m):
+            constraint = test_constraints[i]
+            for j in range(m):
+                x = x_values[j]
+                for constraint_index in range(len(constraint)):
+                    constraint_term = constraint[constraint_index]
+                    for index in range(2):
+                        if x == constraint_term[index]:
+                            label_to_add = constraint_term[(index + 1) % 2]
+                            jacobian[i, j] = complex(state[label_to_add]) * ((-1) ** constraint_index)
+
+        rank = np.linalg.matrix_rank(jacobian)
+        if rank == m:
+            independent_constraints.append(all_constraints[z])
+        # if z % 1000 == 0:
+        #     print(f'Constraint number {z} reached')
+
+    return independent_constraints
+
+
+def get_constraints_seen_for_targets(
+        all_constraints: List[np.ndarray],
+        indexes: List[int],
+        state: np.ndarray,
+        dim: int,
+        number_of_runs: int
+) -> set:
+    all_seen_constraints = set()
+    targets = [tuple(map(tuple, constraint)) for constraint in [all_constraints[x] for x in indexes]]
+    for count in range(number_of_runs):
+        random.shuffle(all_constraints)
+        independent_constraints = find_independent_constraints(all_constraints, state)
+        # print(f'Number of independent constraints = {len(independent_constraints)}')
+
+        long_constraints = [constraint for constraint in independent_constraints if len(constraint) == dim]
+        # print(f'{len(long_constraints)} highest order constraints')
+
+        to_add = [False] * len(targets)
+        for constraint in long_constraints:
+            mapped = tuple(map(tuple, constraint))
+            for index, t in enumerate(targets):
+                if t == mapped:
+                    to_add[index] = True
+        if all(to_add):
+            for constraint in long_constraints:
+                all_seen_constraints.add(tuple(map(tuple, constraint)))
+    return all_seen_constraints
+
+
+def verify_constraints(constraints: List[np.ndarray], state: np.ndarray) -> None:
+    for cons in constraints:
+        val = 0
+        for j in range(len(cons)):
+            term = cons[j]
+            val += ((-1) ** j) * state[term[0]] * state[term[1]]
+        if abs(val) > 10 ** (-12):
+            print(cons)
+            print(val)
+            raise Exception('Constraint not satisfied')
--- RunGaussianConstraints.py
+++ RunGaussianConstraints.py
@@ -1,18 +1,18 @@
-from ..Constraints.GaussianConstraints import *
+from Code.Constraints.GaussianConstraints import *
+from Code.States.GaussianStates import gaussian_states
 
-for dim in range(4, 5):
-    N = 2 ** dim
-    constraints = get_highest_order_constraints(dim, 1)
-    print(f'Number of constraints = {np.linalg.matrix_rank(constraints[:, :, 0])}')
-    print(constraints[0, :, :])
+for dim in range(6, 7, 2):
+    all_constraints = get_all_constraints(dim)
+    state = gaussian_states(1, dim)
+    verify_constraints(all_constraints, state)
 
-    if dim < 8:
-        _, indep_rows = sympy.Matrix(constraints[:, :, 0]).T.rref()
-        unique_constraints = constraints[indep_rows, :, 0]
-        verify_highest_order_constraints(dim, unique_constraints)
-        print(unique_constraints)
+    # print(f'dim = {dim}')
+    # print(f'No. constraints = {len(all_constraints)}')
 
-    for number_of_differing_bits in range(4, dim - 1, 2):
-        lower_order_constraints = get_lower_order_constraints(dim, number_of_differing_bits)
-        print(lower_order_constraints)
-# TODO: calculate the lower order constraints
+    get_constraints_seen_for_targets(
+        all_constraints,
+        indexes=[0, 2, 4, 7],
+        state=state,
+        dim=dim,
+        number_of_runs=800
+    )
