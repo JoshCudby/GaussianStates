7210f6cd801edfb225f8381e836f32e074181673
Josh Cudby <jjcc2@cam.ac.uk>
2022-10-26 15:34:53 +0100

    Improve the direct algorithm

--- Code/Constraints/IndependentConstraintsDirectAlgorithm.py
+++ Code/Constraints/IndependentConstraintsDirectAlgorithm.py
@@ -5,7 +5,7 @@ from ..States.GaussianStates import gaussian_states
 from ..Utils.FileReading import *
 import numpy as np
 from typing import List
-import random
+from time import time
 import os
 
 logger = get_formatted_logger(__name__)
@@ -18,13 +18,17 @@ def change_i_bit(string: np.ndarray, i: int) -> np.ndarray:
 
 
 def get_targets(n: int) -> List[List[np.ndarray]]:
-    other_parity_strings = [item for sublist in
-                            [strings_with_weight(n, k) for k in range((n + 1) % 2, n, 2)]
-                            for item in sublist]
-    targets = [[other_parity_strings[i], other_parity_strings[j]]
-               for i in range(len(other_parity_strings))
-               for j in range(i + 1, len(other_parity_strings))
-               if hamming(other_parity_strings[i], other_parity_strings[j]) * n > 2]
+    other_parity_strings = [
+        item for sublist in
+        [strings_with_weight(n, k) for k in range((n + 1) % 2, n, 2)]
+        for item in sublist
+    ]
+    targets = [
+        [other_parity_strings[i], other_parity_strings[j]]
+        for i in range(len(other_parity_strings))
+        for j in range(i + 1, len(other_parity_strings))
+        if hamming(other_parity_strings[i], other_parity_strings[j]) * n > 2
+    ]
     return targets
 
 
@@ -41,50 +45,72 @@ def get_constraints_from_targets(targets: List[List[np.ndarray]]) -> List[np.nda
     return constraints
 
 
+def differentiate_constraint_with_state(constraint: np.ndarray, x_value: int, state: np.ndarray):
+    return np.sum([
+        complex(state[constraint[constraint_index][(index + 1) % 2]] * (-1) ** constraint_index)
+        if constraint[constraint_index][index] == x_value else 0
+        for index in range(2)
+        for constraint_index in range(len(constraint))])
+
+
 def get_independent_set_of_constraints(constraints: List[np.ndarray], n: int):
+    start_time = time()
     independent_constraints = []
     state = gaussian_states(1, n)
-
-    x_values_set = set()
+    x_values = []
+    jacobian = None
     for z in range(len(constraints)):
-        test_constraints = independent_constraints.copy()
-        test_constraints.append(constraints[z])
-        m = len(test_constraints)
+        if time() - start_time > 30:
+            logger.info(f'Currently on iteration {z} out of {len(constraints)}.'
+                        f'\n Current matrix size is {len(independent_constraints)}.')
+            start_time = time()
+        new_test_constraint = constraints[z]
+        m = len(independent_constraints) + 1
 
-        new_a_labels = constraints[z].flatten()
-        test_x_values = list(x_values_set.copy())
+        new_a_labels = new_test_constraint.flatten()
+        test_x_values = None
+        new_test_x_value = None
+
         for a in new_a_labels:
-            if a not in x_values_set:
-                test_x_values.append(a)
+            if a not in x_values:
+                new_test_x_value = a
+                test_x_values = x_values + [a]
                 break
 
-        if m > len(test_x_values):
-            flattened_constraints = [constraint.flatten() for constraint in test_constraints]
+        if new_test_x_value is None:
+            flattened_constraints = [constraint.flatten() for constraint in independent_constraints]
             a_labels = [item for sublist in flattened_constraints for item in sublist]
-            # random.shuffle(a_labels)
             for a in a_labels:
-                if a not in x_values_set:
-                    test_x_values.append(a)
+                if a not in x_values:
+                    new_test_x_value = a
+                    test_x_values = x_values + [a]
                     break
 
-        if m > len(test_x_values):
-            raise Exception('Not enough a values')
+        if new_test_x_value is None:
+            raise Exception('Could not find a new a label to differentiate w.r.t.')
 
-        jacobian = np.array(
-            [[np.sum([
-                complex(state[constraint[constraint_index][(index + 1) % 2]] * (-1) ** constraint_index)
-              if constraint[constraint_index][index] == test_x_values[j] else 0
-                for index in range(2)
-                for constraint_index in range(len(constraint))])
-                for j in range(m)]
-                for constraint in test_constraints],
-            dtype=complex
-        )
+        # Handle the special case for the first loop
+        if jacobian is None:
+            value = differentiate_constraint_with_state(new_test_constraint, new_test_x_value, state)
+            new_jacobian = np.array([[value]])
+        else:
+            column_to_add = [
+                differentiate_constraint_with_state(constraint, new_test_x_value, state)
+                for constraint in independent_constraints
+            ]
+            new_jacobian = np.column_stack((jacobian, column_to_add))
 
-        rank = np.linalg.matrix_rank(jacobian)
+            row_to_add = [
+                differentiate_constraint_with_state(new_test_constraint, test_x_values[j], state)
+                for j in range(m)
+            ]
+            new_jacobian = np.vstack((new_jacobian, row_to_add))
+
+        rank = np.linalg.matrix_rank(new_jacobian)
         if rank == m:
-            independent_constraints.append(constraints[z])
-            x_values_set = set(test_x_values)
+            independent_constraints.append(new_test_constraint)
+            x_values = test_x_values
+            jacobian = new_jacobian
     return independent_constraints
 
 
--- RunIndependentConstraintsDirect.py
+++ RunIndependentConstraintsDirect.py
@@ -5,9 +5,9 @@ from Code.Utils.Logging import get_formatted_logger
 from time import time
 
 logger = get_formatted_logger(__name__)
-should_overwrite = True
+should_overwrite = False
 
-for dim in range(4, 9, 2):
+for dim in range(6, 11, 2):
     state = gaussian_states(1, dim)
     filename = f'./Output/IndependentConstraints/independent_constraints_{dim}.npy'
     try:
@@ -16,7 +16,7 @@ for dim in range(4, 9, 2):
         independent_constraints = load_list_np_array(filename)
         logger.info(f'Loaded existing constraints for n = {dim}')
     except FileNotFoundError:
-        logger.info(f'No existing constraints found for n = {dim}. Computing ...')
+        logger.info(f'Computing constraints for n = {dim} ...')
         start_time = time()
         independent_constraints = get_independent_constraints_directly(dim)
         logger.info('Computed constraints')
