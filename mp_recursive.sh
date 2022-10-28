df6413f81b0a6362b06ea05098ee16ea091c9d9c
Josh Cudby <jjcc2@cam.ac.uk>
2022-10-28 15:27:50 +0100

    Make shell script for MP

--- mp_recursive.sh
+++ mp_recursive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #! Name of the job:
-#SBATCH -J recursive_indep_only
+#SBATCH -J multithread_recursive
 #! Partition
 #!SBATCH -p skylake
 #! Number of tasks
 #SBATCH --ntasks-per-node=1
 #! Number of cores per task (use for multithreaded jobs, by default 1)
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 18
 #!How much wallclock time will be required (HH:MM:SS)
 #SBATCH --time=12:00:00
 #!Output file name
@@ -15,4 +15,4 @@
 #!SBATCH --mem-per-cpu=64G
 
 source activate python38-env
-srun python3 RunRecursiveConstraintsIndepOnly.py
+srun python3 RunMpRecursive.py
