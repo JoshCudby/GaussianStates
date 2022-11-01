1458eaa5da0953e42f5182554b8f4990747a7a3c
Josh Cudby <jjcc2@cam.ac.uk>
2022-11-01 11:49:10 +0000

    Update shell script

--- Code/ShellScripts/gaussian_rank.sh
+++ Code/ShellScripts/gaussian_rank.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #! Name of the job:
-#SBATCH -J recursive_indep_only
+#SBATCH -J gaussian_rank
 #! Partition
 #!SBATCH -p skylake
 #! Number of tasks
@@ -10,9 +10,9 @@
 #!How much wallclock time will be required (HH:MM:SS)
 #SBATCH --time=12:00:00
 #!Output file name
-#SBATCH --output=./Slurm/GaussianRank/R-%x.%j.out
+#SBATCH --output=Slurm/GaussianRank/R-%x.%j.out
 #! Use default memory for now
 #!SBATCH --mem-per-cpu=64G
 
 source activate python38-env
-srun python3 ../Runners/RunFindGaussianRank.py
+srun python3 Code/Runners/RunFindGaussianRank.py
