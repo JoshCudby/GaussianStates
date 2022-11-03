5c28d189e60a6d30106f59d6c104fe3194a57fef
Josh Cudby <jjcc2@cam.ac.uk>
2022-11-03 12:17:11 +0000

    Try with real and imag parts split

--- gaussianstates/gaussianrank/gaussian_rank_magic.py
+++ gaussianstates/gaussianrank/gaussian_rank_magic.py
@@ -18,37 +18,41 @@ def find_gaussian_rank_magic():
     # n = 1
     input = magic_state()
     chi = 2
-    logger.info(f'Trying to find a decomposition of rank {chi}')
+    # TODO: make this loop terminate with some sensible condition
     while True:
+        logger.info(f'Trying to find a decomposition of rank {chi}')
         x_vars = sympy.Array([list(sympy.symbols('x%d(0:%d)' % (i, 4), real=True)) for i in range(chi)])
         y_vars = sympy.Array([list(sympy.symbols('y%d(0:%d)' % (i, 4), real=True)) for i in range(chi)])
         a_vars = sympy.Array([[x_vars[i][j] + 1j * y_vars[i][j] for j in range(4)] for i in range(chi)])
-        lambda_vars = sympy.Array([list(sympy.symbols('L%d(0:%d)' % (i, 3), real=True)) for i in range(chi)])
+        a_star_vars = sympy.Array([[x_vars[i][j] - 1j * y_vars[i][j] for j in range(4)] for i in range(chi)])
+        lambda_vars = sympy.Array([list(sympy.symbols('L%d(0:%d)' % (i, 2), real=False)) for i in range(chi)])
         all_vars = [item for sublist in x_vars.tolist() + y_vars.tolist() + lambda_vars.tolist() for item in sublist]
 
-        lagrangian = 0.25 * sum([
-            (a_vars[i][k1]) * (a_vars[i][k2])
+        lagrangian = 1 / 4 * sum([
+            (a_vars[i][k1]) * (a_star_vars[i][k2])
             for i in range(chi) for k1 in range(4) for k2 in range(4)
         ])
-        lagrangian -= sum([
+        lagrangian += sum([
             lambda_vars[i][0] * (a_vars[i][0] * a_vars[i][3] + a_vars[i][1] * a_vars[i][2])
             + lambda_vars[i][1] * (sum([x_vars[i][k] ** 2 + y_vars[i][k] ** 2 for k in range(4)]) - 1)
-            + lambda_vars[i][2] * (y_vars[i][0])
             for i in range(chi)
         ])
         derivatives = [lagrangian.diff(v) for v in all_vars]
-        logger.info(lagrangian)
-        logger.info(derivatives)
-        solutions = sympy.nonlinsolve(derivatives, all_vars)
-        logger.info(solutions)
-        if chi > 2:
-            return False
+        solutions = sympy.solve(derivatives, all_vars)
+        values = [lagrangian.subs([(all_vars[i], solution[i]) for i in range(len(all_vars))]) for solution in solutions]
+        deriv_values = [derivative.subs([(all_vars[i], solution[i]) for i in range(len(all_vars))]) for solution in solutions for derivative in derivatives]
+        # logger.info(solutions)
+        logger.info(values)
+        logger.info(deriv_values)
+        if chi > 1:
+            return lagrangian, derivatives, values, solutions
         chi += 1
 
 
 def main():
-    find_gaussian_rank_magic()
+    lagrangian, derivatives, values, solutions = find_gaussian_rank_magic()
+    return lagrangian, derivatives, values, solutions
 
 
 if __name__ == '__main__':
-    main()
+    L, D, V, S = main()
