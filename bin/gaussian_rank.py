8e8dc026b6f8d8eea2b4a439d7ac173e8ee0609a
Josh Cudby <jjcc2@cam.ac.uk>
2022-12-07 09:22:00 +0000

    File handling improvements

--- bin/gaussian_rank.py
+++ bin/gaussian_rank.py
@@ -11,20 +11,17 @@ from gaussianstates.utils import logging_utils
 if __name__ == '__main__':
     logger = logging_utils.get_formatted_logger(__name__)
     logger.info(f'Computing gaussian rank ...')
-
     start_time = time.time()
 
     solution = gaussian_rank_magic.main()
 
     logger.info('Computed rank')
-    logger.info('Solution')
-    logger.info(solution.x)
     logger.info('Optimality')
     logger.info(solution.optimality)
     logger.info('Constraint violation')
     logger.info(solution.constr_violation)
     logger.info(f'Execution time: {round(time.time() - start_time, 2)}')
 
-    with open('data/GaussianRank/gaussian_rank_n_8', 'wb') as f:
-        # Pickle the 'data' dictionary using the highest protocol available.
-        pickle.dump(solution, f, pickle.HIGHEST_PROTOCOL)
+    filename = f'data/GaussianRank/gaussian_rank_n_8_{round(time.time(), None)}'
+    with open(filename, 'wb') as f:
+        pickle.dump('solution', f, pickle.HIGHEST_PROTOCOL)
